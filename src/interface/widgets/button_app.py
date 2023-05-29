8bac94db319322c0bc9ae7588d456092f8338641
LOISGALLAUD <loisgallaud@hotmail.com>
2023-05-29 11:22:52 +0200

    responsive buttons added a green theme to the buttons

--- src/interface/widgets/button_app.py
+++ src/interface/widgets/button_app.py
@@ -16,9 +16,16 @@ class ButtonApp(Button):
     """
     DEFAULT_BG = "#A91B60"
     ACTIVE_TOGGLE_COLOR = "#2bb5a6"
-    def __init__(self, master=None, **kwargs) -> None:
+    def __init__(self, master=None, custom_theme:str="Red", **kwargs) -> None:
         Button.__init__(self, master, **kwargs)
-        self.configure(font=("System", 12), bg="#A91B60",
-                       fg="#FFFFFF", activebackground="#DE026D",
-                       activeforeground="#FFFFFF", bd=0, highlightthickness=0,
-                       relief="flat", padx=10, pady=10)
+
+        if custom_theme == "Red":
+            self.configure(font=("System", 12), bg="#A91B60",
+                            fg="#FFFFFF", activebackground="#DE026D",
+                            activeforeground="#FFFFFF", bd=0, highlightthickness=0,
+                            relief="flat", padx=10, pady=10)
+        elif custom_theme == "Green":
+            self.configure(font=("System", 12), bg="#057A6D",
+                            fg="#FFFFFF", activebackground="#019887",
+                            activeforeground="#FFFFFF", bd=0, highlightthickness=0,
+                            relief="flat", padx=10, pady=10)
